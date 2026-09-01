f9ca764c4d219e455d01d64445aa041720799504
Yoann Le Montagner <yoann.le-montagner@scisoft.fr>
2026-09-01 12:13:23 +0200

    Comment

--- python/tests/test_VersionConsistency.py
+++ python/tests/test_VersionConsistency.py
@@ -4,6 +4,10 @@ import importlib.metadata
 
 def test_version_consistency() -> None:
 
+    # "Package version" is the version defined in `cpp/hipop/__init__.py`.
     package_version = hipop.__version__
+
+    # "Distribution version" is the version defined in `pyproject.toml`.
     distribution_version = importlib.metadata.version("scisoftdev-tmp-hipop") # Name of the project in `pyproject.toml` TODO change it
+
     assert package_version == distribution_version, "Package version and distribution version must be identical"
